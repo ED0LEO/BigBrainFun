52b944efb90a85d63cfc6db4c2e35f12844f8cf6
ED0LEO <ed0leo@proton.me>
2023-04-02 00:19:02 +0200

    Added a button to toggle between two images and updated the image view to show the correct image based on the toggle state. Also added a @State variable to track the toggle state.

--- BigBrainFun/ContentView.swift
+++ BigBrainFun/ContentView.swift
@@ -1,20 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var text = ""
+    @State private var text = "Next project to accomplish"
+    @State private var showingImage = false
     
     var body: some View {
         VStack {
-            Image(systemName: "person.circle.fill")
-                .resizable()
-                .frame(width: 100, height: 100)
+            if showingImage {
+                            Image(systemName: "link")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                        } else {
+                            Image(systemName: "questionmark")
+                                .resizable()
+                                .frame(width: 100, height: 100)
+                        }
             TextField("Enter text here", text: $text)
                 .textFieldStyle(RoundedBorderTextFieldStyle())
                 .padding()
             Button(action: {
-                // Perform some action here
-            }) {
-                Text("Submit")
+                            showingImage.toggle()
+                        }) {
+                Text("Play")
                     .padding()
                     .cornerRadius(10)
             }
