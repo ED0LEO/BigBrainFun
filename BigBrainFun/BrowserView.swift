8f80f861a015cae0fb96cb68bb773fe0bc306160
ED0LEO <ed0leo@proton.me>
2023-04-13 18:28:00 +0200

    Address panel for browser has beed added.

--- BigBrainFun/BrowserView.swift
+++ BigBrainFun/BrowserView.swift
@@ -69,15 +69,100 @@ struct BrowserView: NSViewRepresentable {
     
     let tabViewController: TabViewController
     
+    // Customizable properties
+    var backgroundColor: NSColor = .white
+    var cornerRadius: CGFloat = 10
+    
     // MARK: NSViewRepresentable
     
     func makeNSView(context: Context) -> NSView {
-        let view = tabViewController.view
-        view.frame.size = NSSize(width: 400, height: 300)
+        let view = NSView()
+        view.frame.size = NSSize(width: 400, height: 400)
+        
+        let stackView = NSStackView()
+        stackView.orientation = .vertical
+        stackView.spacing = 0
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let addressPanel = AddressPanel(tabViewController: tabViewController)
+        addressPanel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let webView = tabViewController.view
+        webView.wantsLayer = true
+        webView.layer?.backgroundColor = backgroundColor.cgColor
+        webView.layer?.cornerRadius = cornerRadius
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        stackView.addArrangedSubview(addressPanel)
+        stackView.addArrangedSubview(webView)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            stackView.topAnchor.constraint(equalTo: view.topAnchor),
+            stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            addressPanel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+        
         return view
     }
     
     func updateNSView(_ nsView: NSView, context: Context) {}
+    
+}
+
+class AddressPanel: NSView, NSTextFieldDelegate {
+    
+    // MARK: Properties
+    
+    let tabViewController: TabViewController
+    let textField = NSTextField()
+    
+    // MARK: Initialization
+    
+    init(tabViewController: TabViewController) {
+        self.tabViewController = tabViewController
+        super.init(frame: .zero)
+        
+        textField.placeholderString = "Enter a URL"
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(textField)
+        
+        NSLayoutConstraint.activate([
+            textField.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 10),
+            textField.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -10),
+            textField.topAnchor.constraint(equalTo: topAnchor, constant: 2),
+            textField.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2),
+            textField.heightAnchor.constraint(equalToConstant: 30)
+        ])
+        
+        textField.delegate = self
+        textField.sendAction(#selector(NSTextFieldDelegate.controlTextDidEndEditing(_:)), to: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: Actions
+    
+    @objc func textFieldDidEndEditing(_ sender: NSTextField) {
+        // Load the entered URL in the web view
+        let urlString = sender.stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
+        if !urlString.isEmpty, let url = URL(string: urlString) {
+            tabViewController.load(url)
+        }
+    }
+    
+    func controlTextDidEndEditing(_ obj: Notification) {
+            guard let textField = obj.object as? NSTextField else { return }
+            let urlString = textField.stringValue.trimmingCharacters(in: .whitespacesAndNewlines)
+            if !urlString.isEmpty, let url = URL(string: urlString) {
+                tabViewController.load(url)
+            }
+        }
     
 }
 
